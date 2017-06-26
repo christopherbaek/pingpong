7c98d192521ea9d08d18a63f1726d219b9a14148
Christopher Baek <christopher.baek@gmail.com>
2017-06-25 22:45:01 -0700

    Add logging to server

--- pingpong/server.py
+++ pingpong/server.py
@@ -1,29 +1,35 @@
-
+import logging
+import pika
 import socket
 import time
 
 
+# logging
+FORMATTER = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+CONSOLE_HANDLER = logging.StreamHandler()
+CONSOLE_HANDLER.setLevel(logging.DEBUG)
+CONSOLE_HANDLER.setFormatter(FORMATTER)
+
+LOGGER = logging.getLogger('server')
+LOGGER.setLevel(logging.DEBUG)
+LOGGER.addHandler(CONSOLE_HANDLER)
+
+
 class PingPongServer(object):
 
-    SERVER_PORT = 5000
+    SERVER_PORT = 9999
     SERVER_SOCKET_BACKLOG = 5
     CLIENT_SOCKET_TIMEOUT_SECONDS = 5
     SERVER_RECEIVE_BUFFER_SIZE = 1024
 
-    def __init__(self,
-                 client_timeout_handler=None,
-                 client_disconnect_handler=None,
-                 client_interrupted_handler=None):
-
-        self._client_timeout_handler = client_timeout_handler
-        self._client_disconnect_handler = client_disconnect_handler
-        self._client_interrupted_handler = client_interrupted_handler
-
+    def __init__(self):
         self._server_socket = None
         self._client_socket = None
         self._running = True
 
     def initialize(self):
+        LOGGER.info('initializing')
 
         # create the server socket
         self._server_socket = socket.socket(
@@ -40,7 +46,10 @@ class PingPongServer(object):
         # set up server socket to listen
         self._server_socket.listen(PingPongServer.SERVER_SOCKET_BACKLOG)
 
+        LOGGER.info('initialization complete')
+
     def run(self):
+        LOGGER.info('starting')
 
         while self._running:
 
@@ -49,34 +58,39 @@ class PingPongServer(object):
                 self._wait_for_client_connection()
 
             # attempt to read client message
+            LOGGER.info('waiting for client message')
+
             try:
                 client_message = self._client_socket.recv(PingPongServer.SERVER_RECEIVE_BUFFER_SIZE)
             except socket.timeout:
-                if self._client_timeout_handler:
-                    self._client_timeout_handler()
+                LOGGER.info('timed out waiting for client message')
+                self._clear_client_socket()
                 break
 
             if client_message == '':
-                if self._client_disconnect_handler:
-                    self._client_disconnect_handler()
+                LOGGER.info('detected client disconnect')
                 self._clear_client_socket()
                 break
 
             if client_message == 'ping':
+                LOGGER.info('received ping message')
+
                 # attempt to send pong
                 message = 'pong ({})'.format(time.time())
+
+                LOGGER.info('sending message: {}', message)
 
                 try:
                     self._client_socket.send(message)
                 except IOError:
-                    if self._client_interrupted_handler:
-                        self._client_interrupted_handler()
                     self._clear_client_socket()
 
     def _client_is_disconnected(self):
         return self._client_socket is None
 
     def _wait_for_client_connection(self):
+        LOGGER.info('waiting for client connection')
+
         (client_socket, client_address) = self._server_socket.accept()
         client_socket.settimeout(PingPongServer.CLIENT_SOCKET_TIMEOUT_SECONDS)
         self._client_socket = client_socket
@@ -94,4 +108,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
