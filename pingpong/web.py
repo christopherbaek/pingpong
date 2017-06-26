56749bec5eca6c7b9e0d303b4762a2f87bdfa8e7
Christopher Baek <christopher.baek@gmail.com>
2017-06-26 11:34:25 -0700

    Just committing as a checkpoint before deleting unnecessary things

--- pingpong/web.py
+++ pingpong/web.py
@@ -21,12 +21,37 @@ LOGGER.setLevel(logging.DEBUG)
 LOGGER.addHandler(CONSOLE_HANDLER)
 
 
+class RabbitMQClient(object):
+
+    def __init__(self):
+        self._credentials = pika.PlainCredentials('pingpongweb', 'pingpongweb')
+        self._parameters = pika.ConnectionParameters('localhost', 5672, '/', self._credentials)
+        self._connection = pika.SelectConnection(
+            self._parameters,
+            self._on_connection_open,
+            stop_ioloop_on_close=False)
+        self._channel = None
+    
+    def _on_connection_open(self, connection):
+        self._connection.channel(on_open_callback=self._on_channel_open)
+    
+    def _on_channel_open(self, channel):
+        self._channel = channel
+        self._channel.basic_consume(self._handle_state_response, queue='state_response')
+
+    def _handle_state_response(self, channel, method, properties, body):
+        LOGGER.info('received state response')
+
+        emit('state_response', body, json=True)
+
+        self._channel.basic_ack(delivery_tag = method.delivery_tag)
+
+
 # The Flask application
 FLASK_APP = Flask(__name__)
 
 # The Socket.IO application
 SOCKET_IO = SocketIO(FLASK_APP)
-
 
 @FLASK_APP.route('/')
 def index():
@@ -41,13 +66,20 @@ def index():
 def handle_state_request():
     LOGGER.info('received state request')
 
+    # RABBIT_MQ_CHANNEL.basic_publish(exchange='', routing_key='state_request', body='')
+
+    # Mock state response
     state = {
         'device_connection_status': 'Connected',
         'device_id': '12345'
     }
 
-    emit('state_response', json.dumps(state), json=True)
+    RABBIT_MQ_CHANNEL.basic_publish(
+        exchange='',
+        routing_key='state_response',
+        body=json.dumps(state))
 
 
 if __name__ == '__main__':
-    SOCKET_IO.run(FLASK_APP, host='0.0.0.0', port=8888)
+    #SOCKET_IO.run(FLASK_APP, host='0.0.0.0', port=8888)
+    RABBIT_MQ_CONNECTION.ioloop.start()
