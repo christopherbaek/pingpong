438107d7389960de48e6d9621dd205865e470dde
Christopher Baek <christopher.baek@gmail.com>
2017-06-25 22:52:11 -0700

    Add logging to web module

--- pingpong/web.py
+++ pingpong/web.py
@@ -2,10 +2,23 @@
 The admin web application
 """
 import json
+import logging
 import pika
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+
+
+# logging
+FORMATTER = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+CONSOLE_HANDLER = logging.StreamHandler()
+CONSOLE_HANDLER.setLevel(logging.DEBUG)
+CONSOLE_HANDLER.setFormatter(FORMATTER)
+
+LOGGER = logging.getLogger('web')
+LOGGER.setLevel(logging.DEBUG)
+LOGGER.addHandler(CONSOLE_HANDLER)
 
 
 # The Flask application
@@ -17,9 +30,8 @@ SOCKET_IO = SocketIO(FLASK_APP)
 
 @FLASK_APP.route('/')
 def index():
-    """
-    The root route of the Flask application
-    """
+    LOGGER.info('received index GET request')
+
     return render_template(
         'index.html',
         application_name='Ping Pong Server Admin')
@@ -27,6 +39,8 @@ def index():
 
 @SOCKET_IO.on('state_request')
 def handle_state_request():
+    LOGGER.info('received state request')
+
     state = {
         'device_connection_status': 'Connected',
         'device_id': '12345'
@@ -37,4 +51,3 @@ def handle_state_request():
 
 if __name__ == '__main__':
     SOCKET_IO.run(FLASK_APP, host='0.0.0.0', port=8888)
-
